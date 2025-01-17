481430cef778291b4b93394db80f2bd38207c9b2
hm.oh <hm.oh@taekwang.com>
2025-01-17 13:39:12 +0900

    test3

--- src/App.js
+++ src/App.js
@@ -2,22 +2,52 @@ import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-}
+const customer = [{
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '나동빈',
+    'birthday': '960508',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '961127',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+]
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
-  );
+    <div>
+      {
+        customer.map(c=>{
+          return(
+            <Customer 
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            /> 
+          );
+        })
+      }
+    </div>
+    );
 }
 
 export default App;
